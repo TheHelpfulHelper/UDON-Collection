083c0135514b9c1eae54c84b7db76158fccfd3bf
TheHelpfulHelper <mirkocrafter@gmail.com>
2020-05-31 00:56:10 +0200

    THH_ChatSystem v2.2

---
+++ THH_ChatSystem/README.md
@@ -0,0 +1,6 @@
+**ChatSystem v2.2**
+
+Purpose: Send chat messages between players.<br/>
+Complexity Level: Moderate
+
+*SETUP:* Drag and drop the '[THH_ChatManager]' prefab from the Prefabs folder into your scene.
---
+++ THH_ChatSystem/Scripts/CS/THH_ChatManager.cs
@@ -0,0 +1,221 @@
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+using System;
+
+public class THH_ChatManager : UdonSharpBehaviour
+{
+    [HideInInspector, UdonSynced(UdonSyncMode.None)]
+    public string CHAT_MESSAGE = "";
+    private string last_ChatMessage = "";
+
+    [HideInInspector]
+    public string MESSAGE = "SAMPLETEXT";
+    
+    private bool wasOwner;
+    private bool reclaiming;
+    private float chatDelay = 0.2f;
+    private float sendEnd;
+    private bool messageSending;
+    private bool locked;
+    private int messageCounter;
+    private bool autoRetry;
+    private float retryInterval = 0.5f;
+    private float retryTime;
+    private int retryCount;
+    private int maxRetries = 30;
+
+    public THH_ChatLog ChatLogManager;
+    public InputField inputField;
+
+    public void InputFieldEndEdit()
+    {
+        string inputString = inputField.text.Trim();
+        if (string.IsNullOrEmpty(inputString))
+        {
+            return;
+        }
+        SendChatMessage(inputString);
+        inputField.ActivateInputField();
+    }
+
+    void ClearInputField()
+    {
+        inputField.text = string.Empty;
+    }
+
+    void SetInputFieldLocked(bool b)
+    {
+        inputField.interactable = !b;
+    }
+    // == API ==================================================================================================//
+    public void SendChatMessage(string message)
+    {
+        message = message.Replace('<', ' ');
+        message = message.Replace('>', ' ');
+        MESSAGE = message;
+        if (ProcessMessage())
+        {
+            OnMessageSentSuccess();
+        }
+        else
+        {
+            OnMessageSentFail();
+        }
+    }
+
+    void OnMessageSentSuccess()
+    {
+        ClearInputField();
+    }
+
+    void OnMessageSentFail()
+    {
+
+    }
+
+    void OnMessageReceived(string message)
+    {
+        ChatLogManager.Log(message);
+    }
+
+    //==================================================================================================//
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        ChatLogManager.Log($"<color=yellow>[System]: Player <color=lime>{player.displayName}</color> has joined.</color>");
+    }
+    public override void OnPlayerLeft(VRCPlayerApi player)
+    {
+        ChatLogManager.Log($"<color=yellow>[System]: Player <color=lime>{player.displayName}</color> has left.</color>");
+    }
+
+    public void Start()
+    {
+        if (Networking.IsMaster)
+        {
+            wasOwner = true;
+        }
+    }
+
+    public void Update()
+    {
+        bool isOwner = Networking.IsOwner(gameObject);
+        if (isOwner ^ wasOwner) 
+        { 
+            wasOwner = !wasOwner; 
+            if (isOwner)
+            {
+                Debug.Log($"<color=green>[THH_ChatManager]</color> You became owner of the ChatManager.");
+                if (reclaiming)
+                { 
+                    reclaiming = false;
+                    return;
+                }
+                sendEnd = Time.timeSinceLevelLoad + chatDelay;
+                messageSending = true;
+            }
+            else
+            {
+                Debug.Log($"<color=green>[THH_ChatManager]</color> You lost ownership of the ChatManager.");
+                locked = false;
+            }
+        }
+
+        if (messageSending)
+        {
+            if (Time.timeSinceLevelLoad > sendEnd)
+            {
+                int messageID = messageCounter + 1;
+                CHAT_MESSAGE = $"{Networking.LocalPlayer.playerId}|{MESSAGE}|{messageID.ToString("X")}";
+                messageSending = false;
+            }
+        }
+
+        if (autoRetry)
+        {
+            if (Time.timeSinceLevelLoad > retryTime)
+            {
+                if (ProcessMessage())
+                {
+                    autoRetry = false;
+                    SetInputFieldLocked(false);
+                    ClearInputField();
+                }
+                else
+                {
+                    retryTime = Time.timeSinceLevelLoad + retryInterval;
+                }
+            }
+        }
+
+        if (CHAT_MESSAGE != last_ChatMessage && !string.IsNullOrEmpty(CHAT_MESSAGE))
+        {
+            string[] M = CHAT_MESSAGE.Split('|');
+            if (Int32.Parse(M[2]) > messageCounter)
+            {
+                string messageSender = VRCPlayerApi.GetPlayerById(Int32.Parse(M[0])).displayName;
+                string messageContent = M[1];
+                string message = $"<<color=lime>{messageSender}</color>>: {messageContent}";
+                OnMessageReceived(message);
+            }
+
+            // Reclaim ownership as master after message has been received
+            if (Networking.IsMaster && !Networking.GetOwner(gameObject).isLocal)
+            {
+                Debug.Log($"<color=green>[THH_ChatManager]</color> Reclaiming ownership as master");
+                reclaiming = true;
+                Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            }
+            last_ChatMessage = CHAT_MESSAGE;
+        }
+    }
+
+    bool ProcessMessage()
+    {
+        if (locked)
+        {
+            ChatLogManager.Log($"<i><color=red>You are sending messages too quickly</color></i>");
+            return false;
+        }
+        if (Networking.GetOwner(gameObject).isMaster)
+        {
+            if (Networking.IsMaster)
+            {
+                int messageID = messageCounter + 1;
+                CHAT_MESSAGE = $"{Networking.LocalPlayer.playerId}|{MESSAGE}|{messageID.ToString("X")}";
+                return true;
+            }
+            else
+            {
+                locked = true;
+                Networking.SetOwner(Networking.LocalPlayer, gameObject);
+                return true;
+            }
+        }
+        else
+        {
+            if (autoRetry)
+            {
+                retryCount++;
+                if (retryCount == maxRetries)
+                {
+                    Debug.LogError($"<color=green>[THH_ChatManager]</color> Message could not be delivered after {retryCount} retries, aborting...");
+                    ChatLogManager.Log($"<i><color=red>Your message could not be delivered</color></i>");
+                    autoRetry = false;
+                    retryCount = 0;
+                    SetInputFieldLocked(false);
+                }
+                return false;
+            }
+            Debug.Log($"<color=green>[THH_ChatManager]</color> The chat manager is currently occupied by someone else, retrying...");
+            autoRetry = true;
+            retryTime = Time.timeSinceLevelLoad + retryInterval;
+            SetInputFieldLocked(true);
+            return false;
+        }
+    }
+}
